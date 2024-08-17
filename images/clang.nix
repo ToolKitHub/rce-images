8255e5ea145b33267ffb8fdb739062d6976cec73
xosnrdev <successxodev@gmail.com>
2024-08-17 07:09:23 -0400

    fix: Update image names and references to use "clang" instead of "c"

--- images/clang.nix
+++ images/clang.nix
@@ -7,7 +7,7 @@ let
 in
 build_image {
   pkgs = pkgs;
-  name = "ghcr.io/toolkithub/rce-images-c";
+  name = "ghcr.io/toolkithub/rce-images-clang";
 
   installedPackages = [
     pkgs.clang
--- scripts/build_batch_10.sh
+++ scripts/build_batch_10.sh
@@ -4,5 +4,5 @@ set -e
 mkdir -p result
 nix-build --out-link result/rce-images-ruby images/ruby.nix
 nix-build --out-link result/rce-images-rust images/rust.nix
-nix-build --out-link result/rce-images-sac images/saclang.nix
+nix-build --out-link result/rce-images-sac images/sac.nix
 nix-build --out-link result/rce-images-scala images/scala.nix
