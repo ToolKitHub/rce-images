0e8f02654d555b497193f604411332fd01c1623d
Success Kingsley <xosnrdev@gmail.com>
2025-05-24 19:29:01 -0400

    fix: update rce-runner revision to latest commit

--- src/common/rce_runner.nix
+++ src/common/rce_runner.nix
@@ -4,7 +4,7 @@ let
   rceRunnerSrc = builtins.fetchGit {
     url = "https://github.com/ToolKitHub/rce-runner.git";
     ref = "main";
-    rev = "43af3e01c2d76146d81c816d7c3623d5518b9ea8";
+    rev = "b946780a89b81f01c688eb7a5a161dab1095a287";
   };
 
   rceRunner = import "${rceRunnerSrc}/Cargo.nix" { pkgs = pkgs; };
