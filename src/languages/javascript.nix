cc11e15c18080a2355cda6bd9db842a044fd47d5
Success Kingsley <xosnrdev@gmail.com>
2025-05-24 21:32:17 -0400

    chore: organize folders and imports

--- src/languages/javascript.nix
+++ src/languages/javascript.nix
@@ -3,7 +3,7 @@ let
 
   build_image = import ../common/build_image.nix;
 
-  rceNodeModules = import ./javascript/rce-node-modules/default.nix { inherit pkgs; };
+  rceNodeModules = import ./javascript/default.nix { inherit pkgs; };
 in
 build_image {
   inherit pkgs;
@@ -11,7 +11,7 @@ build_image {
 
   installedPackages = [ pkgs.nodejs ];
   env = [
-    "PATH=${pkgs.nodejs}/bin/:${rceNodeModules}/libexec/rce-node-modules/node_modules/.bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-    "NODE_PATH=${rceNodeModules}/libexec/rce-node-modules/node_modules"
+    "PATH=${pkgs.nodejs}/bin/:${rceNodeModules}/libexec/node_modules/.bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+    "NODE_PATH=${rceNodeModules}/libexec/node_modules"
   ];
 }
