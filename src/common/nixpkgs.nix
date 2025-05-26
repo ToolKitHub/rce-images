090a6a89b9f5de68701ccb420678d86585b7530b
Success Kingsley <xosnrdev@gmail.com>
2025-05-26 07:29:32 -0400

    fix: revert nixpkgs reference to release-24.05

--- src/common/nixpkgs.nix
+++ src/common/nixpkgs.nix
@@ -1,7 +1,8 @@
 let
   nixpkgs = builtins.fetchGit {
     url = "https://github.com/NixOS/nixpkgs";
-    ref = "refs/heads/nixpkgs-unstable";
+    ref = "refs/heads/release-24.05";
+    rev = "0da3c44a9460a26d2025ec3ed2ec60a895eb1114";
   };
 
   pkgs = import nixpkgs { };
