e9d80eedecde0fc865634378dc9e7b1184190ad3
Success Kingsley <xosnrdev@gmail.com>
2025-05-26 07:42:56 -0400

    fix: back to nixpkgs-unstable

--- src/common/nixpkgs.nix
+++ src/common/nixpkgs.nix
@@ -1,8 +1,7 @@
 let
   nixpkgs = builtins.fetchGit {
     url = "https://github.com/NixOS/nixpkgs";
-    ref = "refs/heads/release-24.05";
-    rev = "0da3c44a9460a26d2025ec3ed2ec60a895eb1114";
+    ref = "refs/heads/nixpkgs-unstable";
   };
 
   pkgs = import nixpkgs { };
